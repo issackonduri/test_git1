5616bb99a2cabc64d62eb6c6697f6a31e4baecdd
issac konduri <issackonduri@gmail.com>
2024-03-01 13:11:32 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,5 +2,7 @@ hungry = input("are you hungry")
 if hungry == "yes":
     print("eat biryani")
     print("eat pizza")
+    print("eat burger")
+    print("eat fries")
 else:
     print("do your work")
